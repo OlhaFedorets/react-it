24d1161d1dee72e1fdde47c9d01a1ff044c8e3ea
OlhaFedorets <fedoretsolha@gmail.com>
2024-12-16 10:56:44 +0300

    change accordion

---
+++ src/components/accordion/Accordion.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+export function Accordion() {
+    return (
+        <div>
+            <AccordionTitle/>
+            <AccordionBody/>
+        </div>
+    )
+}
+
+export function AccordionTitle() {
+    return (
+        <h3>Меню</h3>
+    )
+}
+
+export function AccordionBody() {
+    return (
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    )
+}
