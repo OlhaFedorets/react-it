d367730a5abfd33a635487457cb60d5f2e4da118
OlhaFedorets <fedoretsolha@gmail.com>
2024-12-19 10:51:19 +0300

    change app

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,32 @@ import React from 'react';
 import './App.css';
 import {Accordion} from "./components/accordion/Accordion";
 import {Rating} from "./components/rating/Rating";
+import {OnOff} from "./components/onOff/OnOff";
+import {UncontrolledAccordion} from "./components/uncontrolledAccordion/UncontrolledAccordion";
+import {UncontrolledRating} from "./components/uncontrolledRating/UncontrolledRating";
 
 function App() {
     return (
         <div className="App">
-            <AppTitle/>
-            <Rating value={3}/>
-            <Accordion title={'Меню'}/>
-            <Rating value={0}/>
-            <Rating value={1}/>
-            <Rating value={2}/>
-            <Rating value={3}/>
-            <Rating value={4}/>
-            <Rating value={5}/>
+            <OnOff />
+
+            <UncontrolledAccordion title={'Menu'}/>
+            <UncontrolledAccordion title={'Users'}/>
+
+            <UncontrolledRating />
+
+            {/*<Accordion title={'Users'} collapsed={true}/>*/}
+            {/*<Accordion title={'Menu'} collapsed={false}/>*/}
+
+            {/*<AppTitle/>*/}
+
+
+            {/*<Rating value={0}/>*/}
+            {/*<Rating value={1}/>*/}
+            {/*<Rating value={2}/>*/}
+            {/*<Rating value={3}/>*/}
+            {/*<Rating value={4}/>*/}
+            {/*<Rating value={5}/>*/}
         </div>
     );
 }
