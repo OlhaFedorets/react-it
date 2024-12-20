b9bd2f4023589e81dd9c8d244e53a447f1702663
OlhaFedorets <fedoretsolha@gmail.com>
2024-12-20 16:14:01 +0300

    change uncontrolledRating

--- src/components/uncontrolledRating/UncontrolledRating.tsx
+++ src/components/uncontrolledRating/UncontrolledRating.tsx
@@ -6,22 +6,13 @@ export function UncontrolledRating(props: RatingPropsType) {
 
     const [value, setValue] = useState<number>(0)
 
-    const changeValue = (newValue: number) => {
-        setValue(newValue)
-    }
-
     return (
         <div>
-            <Star selected={value > 0} changeValue={changeValue} value={1}/>
-            <Star selected={value > 1} changeValue={changeValue} value={2}/>
-            <Star selected={value > 2} changeValue={changeValue} value={3}/>
-            <Star selected={value > 3} changeValue={changeValue} value={4}/>
-            <Star selected={value > 4} changeValue={changeValue} value={5}/>
-            {/*<Star selected={value > 0}/><button onClick={()=>{setValue(1)}}>1</button>*/}
-            {/*<Star selected={value > 1}/><button onClick={()=>{setValue(2)}}>2</button>*/}
-            {/*<Star selected={value > 2}/><button onClick={()=>{setValue(3)}}>3</button>*/}
-            {/*<Star selected={value > 3}/><button onClick={()=>{setValue(4)}}>4</button>*/}
-            {/*<Star selected={value > 4}/><button onClick={()=>{setValue(5)}}>5</button>*/}
+            <Star selected={value > 0} setValue={() => setValue(1)}/>
+            <Star selected={value > 1} setValue={() => setValue(2)}/>
+            <Star selected={value > 2} setValue={() => setValue(3)}/>
+            <Star selected={value > 3} setValue={() => setValue(4)}/>
+            <Star selected={value > 4} setValue={() => setValue(5)}/>
         </div>
     )
 }
@@ -29,20 +20,13 @@ export function UncontrolledRating(props: RatingPropsType) {
 
 type StarPropsType = {
     selected: boolean
-    changeValue: (newValue: number) => void
-    value: 1 | 2 | 3 | 4 | 5
+    setValue: () => void
 }
 
 function Star(props: StarPropsType) {
 
-    // const onClickHandler = () => {
-    //     props.changeValue(props.value)
-    // }
-    //
-    //     return props.selected ? <span onClick={onClickHandler}><b>star</b> </span> :
-    //     <span onClick={onClickHandler}>star </span>
+    return <span onClick={props.setValue}>
+        {props.selected ? <b>star </b> : 'star '}
+    </span>
 
-    return props.selected
-        ? <span onClick={() => props.changeValue(props.value)}><b>star</b> </span>
-        : <span onClick={() => props.changeValue(props.value)}>star </span>
 }
