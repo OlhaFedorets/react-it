107ebf46eecffbfd9694a6d374a361456f702744
OlhaFedorets <fedoretsolha@gmail.com>
2024-12-20 10:56:45 +0300

    change uncontrolledAccordion

--- src/components/uncontrolledAccordion/UncontrolledAccordion.tsx
+++ src/components/uncontrolledAccordion/UncontrolledAccordion.tsx
@@ -8,10 +8,14 @@ export function UncontrolledAccordion(props: AccordionPropsType) {
 
     const [collapsed, setCollapsed] = useState(true)
 
+    const changeCollapsed = () => {
+        setCollapsed(!collapsed)
+    }
+
     return (
         <div>
-            <AccordionTitle titleValue={props.title}/>
-            <button onClick={()=>{setCollapsed(!collapsed)}}>TOGGLE</button>
+            <AccordionTitle titleValue={props.title} changeCollapsed={changeCollapsed}/>
+            {/*<button onClick={()=>{setCollapsed(!collapsed)}}>TOGGLE</button>*/}
             {!collapsed && <AccordionBody/>}
         </div>
     )
@@ -20,11 +24,18 @@ export function UncontrolledAccordion(props: AccordionPropsType) {
 
 type AccordionTitlePropsType = {
     titleValue: string
+    changeCollapsed: () => void
 }
 
 function AccordionTitle(props: AccordionTitlePropsType) {
+
+    // const onClickHandler = () => {
+    //     props.changeCollapsed()
+    // }
+
     return (
-        <h3>{props.titleValue}</h3>
+        <h3 onClick={props.changeCollapsed}>---{props.titleValue}---</h3>
+        // <h3 onClick={onClickHandler}>---{props.titleValue}---</h3>
     )
 }
 
