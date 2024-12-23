90aa78fae37f1715a34f940b7715d6a8ce234031
OlhaFedorets <fedoretsolha@gmail.com>
2024-12-23 11:18:42 +0300

    change Rating.stories.tsx

--- src/components/rating/Rating.stories.tsx
+++ src/components/rating/Rating.stories.tsx
@@ -1,7 +1,8 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
 
-import {Rating} from "./Rating";
+import {Rating, RatingValueType} from "./Rating";
+import {useState} from "react";
 
 export default {
     title: 'Rating stories',
@@ -15,4 +16,8 @@ export const Rating2 = () => <Rating value={2} onClick={x=>x}/>
 export const Rating3 = () => <Rating value={3} onClick={x=>x}/>
 export const Rating4 = () => <Rating value={4} onClick={x=>x}/>
 export const Rating5 = () => <Rating value={5} onClick={x=>x}/>
+export const RatingChanging = () => {
+    const [rating, setRating] = useState<RatingValueType>(5);
+    return <Rating value={rating} onClick={setRating}/>
+}
 
