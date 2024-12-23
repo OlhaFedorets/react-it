70b52361c932ec9c218f5526b228cb17c3490f4c
OlhaFedorets <fedoretsolha@gmail.com>
2024-12-23 17:16:39 +0300

    change Accordion.stories.tsx

---
+++ src/components/accordion/Accordion.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { action } from '@storybook/addon-actions'
+
+
+import {useState} from "react";
+import {Accordion} from "./Accordion";
+
+export default {
+    title: 'Accordion',
+    component: Accordion,
+}
+
+const callback = action('accordion mode change event fired')
+
+export const MenuCollapsedMode = () => <Accordion collapsed={true} onClick={callback} title={'Menu'}/>
+export const UsersUnCollapsedMode = () => <Accordion collapsed={false} onClick={callback} title={'Users'}/>
+
+export const ModeChanging = () => {
+    const [value, setValue] = useState<boolean>(true);
+    return <Accordion collapsed={value} onClick={()=>setValue(!value)} title={'Changing mode'}/>
+}
+
