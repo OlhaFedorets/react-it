3ce36f9838b8850d5f0a8c7e8a303d3eebfeee8d
OlhaFedorets <fedoretsolha@gmail.com>
2024-12-25 13:58:52 +0300

    change OnOff.stories.tsx

--- src/components/onOff/OnOff.stories.tsx
+++ src/components/onOff/OnOff.stories.tsx
@@ -1,7 +1,4 @@
-import type { Meta, StoryObj } from '@storybook/react';
-import { action } from '@storybook/addon-actions'
-
-
+import {action} from '@storybook/addon-actions'
 import {useState} from "react";
 import {OnOff} from "./OnOff";
 
@@ -16,7 +13,7 @@ export const OnMode = () => <OnOff on={true} onClick={callback}/>
 export const OffMode = () => <OnOff on={false} onClick={callback}/>
 
 export const ModeChanging = () => {
-    const [value, seValue] = useState<boolean>(true);
-    return <OnOff on={value} onClick={seValue}/>
+    const [value, setValue] = useState<boolean>(true);
+    return <OnOff on={value} onClick={setValue}/>
 }
 
