46cb78bc3623753a7f73e174f1f96a81d2747f68
OlhaFedorets <fedoretsolha@gmail.com>
2025-01-13 10:09:08 +0300

    change input.stories.tsx

--- src/stories/input.stories.tsx
+++ src/stories/input.stories.tsx
@@ -1,7 +1,5 @@
-import type {Meta, StoryObj} from '@storybook/react';
-import {fn} from '@storybook/test';
 import {ChangeEvent, useRef, useState} from "react";
-import {Button} from "./Button";
+import {action} from "@storybook/addon-actions";
 
 
 export default {
@@ -11,6 +9,7 @@ export default {
 
 
 export const UncontrolledInput = () => <input/>;
+
 export const TrackValueOfUncontrolledInput = () => {
     const [value, setValue] = useState('')
 
@@ -39,6 +38,38 @@ export const TrackValueOfUncontrolledInputByButtonPress = () => {
             - actual value: {value}
         </>
     )
+}
+
+export const ControlledInput = () => {
+    const [parentValue, setParentValue] = useState('')
+
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setParentValue(e.currentTarget.value)
+    }
+    return <input value={parentValue} onChange={onChangeHandler}/>
+}
+
+export const ControlledCheckbox = () => {
+    const [parentValue, setParentValue] = useState(true)
+
+    const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setParentValue(e.currentTarget.checked)
+    }
+    return <input type='checkbox' checked={parentValue} onChange={onChange}/>
+}
+
+export const ControlledSelect = () => {
+    const [parentValue, setParentValue] = useState<string | undefined>(undefined)
+
+    const onChange = (e: ChangeEvent<HTMLSelectElement>) => {
+        setParentValue(e.currentTarget.value)
+    }
+    return <select value={parentValue} onChange={onChange}>
+        <option>none</option>
+        <option value={'1'}>Minsk</option>
+        <option value={'2'}>Kiev</option>
+        <option value={'3'}>Moscow</option>
+    </select>
 }
 
 export const ControlledInputWithFixedValue = () => <input value={'it-incubator'}/>;
