fa7228ebcc41ef5bb0f1726b6bc21792a3d7619e
OlhaFedorets <fedoretsolha@gmail.com>
2025-02-04 11:00:51 +0300

    change UseEffect.stories.tsx

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -6,11 +6,13 @@ export default {
 
 export const SimpleExample = () => {
     const [counter, setCounter] = useState(1);
+    const [fake, setFake] = useState(1);
 
     console.log('SIMPLE EXAMPLE')
 
     useEffect(() => {
-        console.log('useEffect');
+        console.log('useEffect every render');
+        document.title = counter.toString()
         // api.getUsers().then
         // setInterval
         // indexedDB
@@ -18,9 +20,21 @@ export const SimpleExample = () => {
         // document.title = 'User'
     })
 
+    useEffect(() => {
+        console.log('useEffect only first render (componentDidmount)');
+        document.title = counter.toString()
+    }, [])
+
+    useEffect(() => {
+        console.log('useEffect first render ane every counter change');
+        document.title = counter.toString()
+    }, [counter])
+
 
     return <>
-        Hello, {counter}
+        Counter: {counter}
         <button onClick={() => setCounter(counter+1)}>+</button>
+        Fake: {fake}
+        <button onClick={() => setFake(fake+1)}>+</button>
     </>
 }
